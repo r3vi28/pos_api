2859b2fd86f1d55288feafe5b217171f046a9720
Moises Javier Revi <moisesjr3vi@gmail.com>
2026-07-23 20:55:00 -0400

    feat: configurar CORS para permitir conexion con el frontend

--- pos_system/settings.py
+++ pos_system/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'drf_yasg',
     'pos',
     'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8100',
+    'http://127.0.0.1:8100',
+]
